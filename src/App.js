2fb39dd85677e25a0d8050e26d7e44222911d3ee
gtretow <guilherme.tretow@gmail.com>
2020-12-23 23:09:45 -0300

    more tests

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div data-test="component-app">
-
-{/* removing data-test attributes */}
-<h1>App</h1>
-
-  </div>;
+  return (
+    <div data-test="component-app">
+      {/* removing data-test attributes */}
+      <h1>App</h1>
+      <h1 data-test="counter-display">Counter is currently</h1>
+      <button data-test="increment-button" />
+    </div>
+  );
 }
 
 export default App;
-
--- src/App.test.js
+++ src/App.test.js
@@ -9,14 +9,29 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
 test("renders without error", () => {
   const wrapper = shallow(<App />);
 
-  const appComponent = wrapper.find("[data-test='component-app']")
+  const appComponent = wrapper.find("[data-test='component-app']");
 
   expect(appComponent.length).toBe(1);
 });
+// first ctrl+c then apply DRY
+test("renders button", () => {
+  const wrapper = shallow(<App />);
 
-test("renders button", () => {});
+  const button = wrapper.find("[data-test='increment-button']");
 
-test("renders counter display", () => {});
+  expect(button.length).toBe(1);
+});
+
+test("renders counter display", () => {
+  const wrapper = shallow(<App />);
+
+  const counterDisplay = wrapper.find("[data-test='counter-display']");
+
+  expect(counterDisplay.length).toBe(1);
+
+
+
+});
 
 test("counter starts at 0", () => {});
 
