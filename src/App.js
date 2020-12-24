cafad55bbd3911a238fa56ebc77f7933979e6d26
gtretow <guilherme.tretow@gmail.com>
2020-12-23 22:32:58 -0300

    started testing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div data-test="component-app"></div>;
+  return <div data-test="component-app">
+
+{/* removing data-test attributes */}
+<h1>App</h1>
+
+  </div>;
 }
 
 export default App;
+
