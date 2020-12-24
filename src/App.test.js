10935e57b611b7babfbd97bbd8a02c79b94b7166
gtretow <guilherme.tretow@gmail.com>
2020-12-23 22:25:24 -0300

    first test pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,17 @@
 import App from "./App";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+import React from "react";
 
 // set up enzyme react adapter
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 test("renders without error", () => {
+  const wrapper = shallow(<App />);
 
-  const wrapper = shallow(<App/>)
+  const appComponent = wrapper.find("[data-test='component-app']")
 
+  expect(appComponent.length).toBe(1);
 });
 
 test("renders button", () => {});
