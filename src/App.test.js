f214b560148cd89a39334ce56b727b9b48943545
gtretow <guilherme.tretow@gmail.com>
2020-12-23 22:10:42 -0300

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from "./App";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// set up enzyme react adapter
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders without error", () => {
+
+  const wrapper = shallow(<App/>)
+
 });
+
+test("renders button", () => {});
+
+test("renders counter display", () => {});
+
+test("counter starts at 0", () => {});
+
+test("clickin on button increments counter display", () => {});
